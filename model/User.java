4d9d2e60e18ec9eb0489cf24973e644dc7de2175
Max <mdondukov@pm.me>
2020-03-09 14:19:10 +0600

    define user model

---
+++ model/User.java
@@ -0,0 +1,87 @@
+package com.example.homework50.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+@Document
+public class User {
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+    private static final Random r = new Random();
+
+    private static String generateId() {
+        return LocalDateTime.now().format(dtf) + r.nextInt();
+    }
+
+    @Id
+    private String id = generateId();
+    private String userName;
+    private String email;
+    private String password;
+    private int publicationsCount;
+    private int subscribesCount;
+    private int subscribersCount;
+
+    public User() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPublicationsCount() {
+        return publicationsCount;
+    }
+
+    public void setPublicationsCount(int publicationsCount) {
+        this.publicationsCount = publicationsCount;
+    }
+
+    public int getSubscribesCount() {
+        return subscribesCount;
+    }
+
+    public void setSubscribesCount(int subscribesCount) {
+        this.subscribesCount = subscribesCount;
+    }
+
+    public int getSubscribersCount() {
+        return subscribersCount;
+    }
+
+    public void setSubscribersCount(int subscribersCount) {
+        this.subscribersCount = subscribersCount;
+    }
+}
