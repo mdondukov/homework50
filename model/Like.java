ef0dd441ea1423cb707ba5628da896a81cdb8db2
Max <mdondukov@pm.me>
2020-03-09 15:24:18 +0600

    define like model

---
+++ model/Like.java
@@ -0,0 +1,48 @@
+package com.example.homework50.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class Like {
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+    private static final Random r = new Random();
+
+    private static String generateId() {
+        return LocalDateTime.now().format(dtf) + r.nextInt();
+    }
+
+    @Id
+    private String id = generateId();
+    @DBRef
+    private User user;
+    private LocalDate pubDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(LocalDate pubDate) {
+        this.pubDate = pubDate;
+    }
+}
