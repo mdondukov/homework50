2761c159e1063710559d6d88ae6250a05d0ccadd
Max <mdondukov@pm.me>
2020-03-09 13:48:37 +0600

    Initial commit

---
+++ Homework50Application.java
@@ -0,0 +1,13 @@
+package com.example.homework50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework50Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework50Application.class, args);
+    }
+
+}
